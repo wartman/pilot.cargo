694eb404e92721f1d63c499c958c949d8ca2a543
wartman <faxmaster@gmail.com>
2019-07-31 11:00:57 -0500

    cleaned up render

--- src/pilot/cargo/ReactiveWidget.hx
+++ src/pilot/cargo/ReactiveWidget.hx
@@ -10,9 +10,9 @@ using pilot.Differ;
 @:autoBuild(pilot.macro.WidgetBuilder.build({ stateful: false }))
 class ReactiveWidget implements Widget {
   
-  var _pilot_link:CallbackLink;
-  var _pilot_observable:Observable<VNode>;
-  var _pilot_vnode:VNode;
+  @:noCompletion var _pilot_link:CallbackLink;
+  @:noCompletion var _pilot_observable:Observable<VNode>;
+  @:noCompletion var _pilot_vnode:VNode;
 
   public function build():VNode {
     throw 'abstract';
@@ -22,19 +22,15 @@ class ReactiveWidget implements Widget {
   public function render():VNode {
     if (_pilot_observable == null) {
       _pilot_observable = Observable.auto(build);
-      _pilot_link = _pilot_observable.bind({ direct: true }, vn -> {
-        if (_pilot_vnode == null) {
-          _pilot_vnode = vn;
-          _pilot_vnode.hooks.detach = () -> {
-            _pilot_link.dissolve();
-            _pilot_vnode = null;
-            _pilot_observable = null;
-          };
-          return;
-        }
-        if (_pilot_vnode.node == null || _pilot_vnode == vn) return;
+      _pilot_vnode = _pilot_observable.value;
+      _pilot_vnode.hooks.detach = () -> {
+        _pilot_link.dissolve();
+        _pilot_vnode = null;
+        _pilot_observable = null;
+      };
+      _pilot_link = _pilot_observable.bind(vn -> {
         _pilot_vnode.subPatch(vn);
-      }); 
+      });
     }
     return _pilot_vnode;
   }
